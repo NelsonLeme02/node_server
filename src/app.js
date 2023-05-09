a3c2e60e7244a85729fc8fc285187c1e5ab048ce
Nelson Leme <leme.nelson02@gmail.com>
2023-05-09 20:33:35 -0300

    inserts

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 'use strict'
 const express = require('express')
-const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
+const path = require('path')
+const fs = require('fs')
+const dbPath = path.resolve(__dirname, './db/facts.json')
+const app = express()
 const port = 3000
 
 app.use(express.json())
@@ -12,8 +15,17 @@ app.use(cors())
 app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-    return res.send('ok')
+    try {
+        const data = fs.readFileSync(dbPath, 'utf8',)
+        const facts = JSON.parse(data)
+        return res.status(200).json(facts)
+    }
+    catch (e) {
+        console.log(e)
+        return res.status(500).json({ erro: 'Erro de execução!' })
+    } 
 })
+
 
 app.listen(port, () => {
   console.log(`Running on ${port}`)
