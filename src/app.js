08a5c2c1c8b281dd743a8a10afb3769b7087860f
Nelson Leme <leme.nelson02@gmail.com>
2023-05-09 19:52:47 -0300

    Npm Installs

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 'use strict'
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
 const port = 3000
 
+app.use(express.json())
+
+app.use(cors())
+
+app.use(morgan('dev'))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    return res.send('ok')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Running on ${port}`)
 })
